89010fb35acbc61904caac583975f6e19c61c3b2
kasagata <k.sagata.1207@gmail.com>
2024-03-11 19:14:55 +0900

    fix: #1 Fix profile_image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :portrait)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_image)
   end
 
   def correct_user

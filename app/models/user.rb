ccae85d9259eef840c981ee5cee4e6b13989da9f
kasagata <k.sagata.1207@gmail.com>
2024-03-11 19:24:22 +0900

    fix: #1 model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one_attached :portrait
+  has_one_attached :profile_image
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }

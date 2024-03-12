a7fdc0fc09add49c425b1427b42d6175dfc749fc
kasagata <k.sagata.1207@gmail.com>
2024-03-12 18:38:20 +0900

    fix: #1 Fix UserMailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.with(to: @user.email).welcome.deliver_later
+      UserMailer.with(to: @user.email, name: @user.name).welcome.deliver_later(wait: 1.minutes) 
       log_in(@user)
       redirect_to user_path(@user.id), notice: 'アカウントを登録しました。'
     else
